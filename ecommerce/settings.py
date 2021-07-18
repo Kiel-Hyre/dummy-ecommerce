07bc6382598b8716913474a2bdca41859ba370a8
Kiel-Hyre <vinceadisip@gmail.com>
2021-07-18 12:36:21 +0800

    async consumer

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '0') == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
--- purchase/consumers.py
+++ purchase/consumers.py
@@ -1,79 +1,84 @@
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async as db_syn_asyn
 
-
-from . import serializers
+from . import models, serializers
 
 
 # from tutorials django-channels
-class PurchaseConsumer(WebsocketConsumer):
+class PurchaseConsumer(AsyncWebsocketConsumer):
 
-    # when connecting
-    def connect(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.receive_methods_dict = {
+            'submit': self.item_add,
+            'remove': self. item_remove,
+        }
+
+    # when connecting, prefer not do list objects here rather through api or
+    # direct render, if ws had reconnecting option it will lead to duplication
+    async def connect(self):
         self.room_group_name = 'purchase-socket'
 
         # join room-group use for layer
-        async_to_sync(self.channel_layer.group_add)(
+        await (self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         data_json = json.loads(text_data)
-        action = data_json.pop('action')
+        action = data_json.pop('action', None)
 
+        func = self.receive_methods_dict.get(action, None)
+        if not func: return
+        await func(data_json)
 
-        # Send message to room group
-        # perform, logic mostly here since if performed in group_send, it will
-        # duplicate
-        from . import models
-        if action == 'submit':
-            name = data_json.get('name', '')
-            serializer = serializers.ItemAddSerializer(data=data_json)
-            if not serializer.is_valid(): return
+    async def item_add(self, data_json, *args, **kwargs):
+        serializer = serializers.ItemAddSerializer(data=data_json)
+        if not serializer.is_valid(): return
 
-            obj = models.Item.objects.create(name=name)
-            created = serializers.ItemSerializer(obj).data
-            created['action'] = 'created'
+        obj = await db_syn_asyn(self.create_item)(name=data_json.get('name', ''))
+        created = serializers.ItemSerializer(obj).data
+        created['action'] = 'created'
 
-            # makes it to apply to other, if direct send only on that ind only
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,{
-                'type': 'item_add', # function
-                'created': created
-            })
+        # makes it to apply to other, if direct send only on that ind only
+        await self.channel_layer.group_send(
+            self.room_group_name,{
+            'type': 'sub_item_add', # function
+            'created': created })
 
-        if action == 'remove':
-            uid = data_json.get('id', None)
-            try: models.Item.objects.get(uid=uid).delete()
-            except Exception as e: return
-
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,{
-                'type': 'item_remove', # function
-                'deleted': {
-                    'id': uid,
-                    'action': 'deleted'
-                }
-            })
+    async def item_remove(self, data_json, *args, **kwargs):
+        uid = data_json.get('id', None)
+        if not await db_syn_asyn(self.delete_item)(uid=uid): return
+        await self.channel_layer.group_send(
+            self.room_group_name,{
+            'type': 'sub_item_remove', # function
+            'deleted': { 'id': uid, 'action': 'deleted'}
+        })
 
     # Receive message from room group
-    def item_add(self, event):
-        self.send(text_data=json.dumps(
-            event['created']
-        ))
+    async def sub_item_add(self, event):
+        await self.send(text_data=json.dumps(event['created']))
 
-    def item_remove(self, event):
-        self.send(text_data=json.dumps(
-            event['deleted']
-        ))
+    async def sub_item_remove(self, event):
+        await self.send(text_data=json.dumps(event['deleted']))
+
+    # db queries, can do in manager
+    def create_item(self, *args, **kwargs):
+        return models.Item.objects.create(**kwargs)
+
+    def delete_item(self, *args, **kwargs):
+        try:return models.Item.objects.filter(**kwargs).delete()
+        except: return False
